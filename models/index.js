dc0f1969c88b7d4035c576ac04c1650648a1d512
shahzaib <zaybb12@gmail.com>
2019-08-17 07:17:09 +0500

    added suport for custom variable env

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const Image = require('./image');
 
-mongoose.connect('mongodb://localhost/Freemage', {
+const DB_URL = process.env.DB_URL || 'mongodb://localhost/Freemage';
+
+mongoose.connect(DB_URL, {
   useNewUrlParser: true
 });
 
