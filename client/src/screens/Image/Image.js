3af7e2b7f1be3f51cb45af367ea98e0c2fdc861d
shahzaib <zaybb12@gmail.com>
2019-09-16 01:20:50 +0500

    added image page

---
+++ client/src/screens/Image/Image.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from '../../components/UI/Button';
+import ImageZoomer from '../../components/UI/ImageZoomer/ImageZoomer';
+import arrow from '../../assets/images/arrow-down.svg';
+import './image.css';
+
+const Image = props => {
+  return (
+    <>
+      <header>
+        <Button title="Download" className="download-btn">
+          <span id="text">Download free</span>
+          <i id="symbol" className="arrow-down">
+            <img src={arrow} alt="" />
+          </i>
+        </Button>
+      </header>
+      <ImageZoomer />
+    </>
+  );
+};
+
+export default Image;
