9809ec9fc4e1e2494362e42af5cf1c1e40c16bb1
shahzaib <zaybb12@gmail.com>
2019-09-25 17:45:05 +0500

    added google oauth api endpoint

--- routes/images.js
+++ routes/images.js
@@ -4,6 +4,7 @@ const multer = require('multer');
 const imageController = require('../controllers/images');
 const cloudinaryUpload = require('../middlewares/cloudinaryUpload');
 const bufferToDataUri = require('../middlewares/bufferToDataUri');
+const auth = require('../middlewares/authenticate');
 
 const router = express.Router();
 
@@ -11,11 +12,12 @@ const upload = multer();
 
 router.post(
   '/',
+  auth,
   [
     upload.single('image'),
     (req, res, next) => {
       if (!req.file.mimetype.match(/^image\/jpe?g$/)) {
-        // return res.status(422).send('invalid file type');
+        return res.status(422).send('invalid file type');
       } else if (req.file.buffer.length >= 5e7) {
         // if greater than 50mb
         return res.status(422).send('file is too large');
