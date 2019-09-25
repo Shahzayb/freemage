bd3881dd6876c932d75604aec65fc01e7b23945e
shahzaib <zaybb12@gmail.com>
2019-09-25 17:30:30 +0500

    updated user model

--- models/User.js
+++ models/User.js
@@ -3,15 +3,25 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   googleId: {
     type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
     required: true
   },
-  username: {
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
     type: String,
     required: true
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   profilePic: {
     type: String,
